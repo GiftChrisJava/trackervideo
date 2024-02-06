8ed02ddbb85869d277605dfabcc4f736f1b2cf83
GiftChrisJava <chrisjava77@gmail.com>
2024-02-06 16:31:57 +0200

    replace

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -6,8 +6,9 @@ export async function GET(req) {
   const cookieStore = cookies();
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
   console.log(req.url, "url in callback");
+  const replacementUrl = originalUrl.replace(/^http:\/\/localhost:3000\//, 'https://trackervideo.vercel.app/');
 
-  const { searchParams } = new URL(req.url);
+  const { searchParams } = new URL(replacementUrl );
 
   const code = searchParams.get("code");
 
@@ -15,5 +16,5 @@ export async function GET(req) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(new URL("/videos", req.url));
+  return NextResponse.redirect(new URL("/videos", replacementUrl ));
 }
