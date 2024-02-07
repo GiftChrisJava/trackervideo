58c94b6a903b4d30f72eac1a9693577892fa2751
GiftChrisJava <chrisjava77@gmail.com>
2024-02-07 08:21:54 +0200

    styling home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,21 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-300">
-      <div className="container mx-auto p-6 sm:p-12">
-        <h1 className="text-5xl md:text-6xl font-extrabold text mb-6">
-          Malawi Edu Enhancement Programs
+    <div className="min-h-screen bg-gray-900 mt-6">
+      <div className="container mx-auto max-w-md p-6 ">
+        <h1 className="text-2xl font-extrabold text mb-6 text-gray-200">
+          Malawi Education Enhancement Program
         </h1>
-        <p className="text-lg md:text-xl text-white mb-6">
+        <p className="text-lg text-gray-300 mb-6 ">
           Expand your knowledge and understand complex lessons taught in class
           by watching videos, reading docs and practice quizez
         </p>
 
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <button>
+        <div className=" flex justify-between">
+          <button className="bg-gray-800 text-gray-200 hover:text-white hover:cursor-pointer font-bold rounded-lg shadow-lg py-2 px-3">
             <a href="/login">Login</a>
           </button>
 
-          <button>
+          <button className="bg-gray-800 text-gray-200 hover:text-white hover:cursor-pointer font-bold rounded-lg shadow-lg py-2 px-3">
             <a href="/register">Register</a>
           </button>
         </div>
