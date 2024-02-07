b99f2a1d2a52fb4b5e8b4d9055a31c06074dcc51
GiftChrisJava <chrisjava77@gmail.com>
2024-02-07 19:51:34 +0200

    supa

--- app/auth/signup/route.js
+++ app/auth/signup/route.js
@@ -17,7 +17,7 @@ export async function POST(req) {
     email,
     password,
     options: {
-      emailRedirectTo: `https://trackervideo.vercel.app/auth/callback/auth/callback`,
+      emailRedirectTo: `https://trackervideo.vercel.app/auth/callback`,
     },
   });
 
